ae1d0a05689c7723b02277a36fc82b0a01b4eaea
andrewsty@gmail.com <andrewsty@gmail.com>
2018-05-14 10:50:45 +0800

    odoo first lesson

---
+++ osbzr_traning/__manifest__.py
@@ -0,0 +1,14 @@
+#-*- encoding: utf-8 -*-
+{
+    'name':'osbzr_traning',
+    'version':'1.0',
+    'author':'6658753@qq.com',
+    'category':'osbzr',
+    'description':
+    """用于学习odoo开发
+    """,
+    'depends':[],
+    'data':[],
+    'auto_install':False
+
+}
