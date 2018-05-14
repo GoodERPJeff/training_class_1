408973dfe61165688d476fc15fa2857481e7dec7
andrewsty@gmail.com <andrewsty@gmail.com>
2018-05-14 10:59:30 +0800

    basic module ï

---
+++ osbzr_traning/__manifest__.py
@@ -0,0 +1,18 @@
+#-*- encoding: utf-8 -*-
+{
+    'name':'odoo课程培训管理',
+    'summary':'好好学习天天向上',
+    'version':'1.0',
+    'author':'6658753@qq.com',
+    'category':'odoo学习',
+    'description':
+    """用于学习odoo开发
+        2018-05-14
+    """,
+    'depends':[],
+    'data':[],
+    'auto_install':False,
+    'website':'https://github.com/GoodERPJeff/training_class_1.git',
+    'installed_version':'0.1',
+    'application':'True'
+}
