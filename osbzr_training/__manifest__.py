6ed05a524a65ecaa6b81b63799b0a6b01aaeb559
andrewsty@gmail.com <andrewsty@gmail.com>
2018-05-16 00:05:35 +0800

    补全_manifest__.py 重构class命名，符合驼峰规范，修改函数命名

--- osbzr_training/__manifest__.py
+++ osbzr_training/__manifest__.py
@@ -2,12 +2,16 @@
 {
     'name': 'osbzr_traning',
     'version': '1.0',
-    'author': 'aa',
+    'author': 'Mr Jeff',
     'category': 'osbzr',
+    'price':'0.99',
+    'summary':'跟jeff学二次开发实操项目',
+    'license':'GPL-2',
     'description':
     """
     用于开阖openerp培训实例
     """,
+    'website':'https://github.com/GoodERPJeff/training_class_1',
     'depends': [],
     'auto_install': False,
     'data':["osbzr_training_view.xml"]
