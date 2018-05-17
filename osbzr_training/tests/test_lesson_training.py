4380e0b7fbb9672d8d4c07e1726697ce8e659098
andrewsty@gmail.com <andrewsty@gmail.com>
2018-05-17 17:04:33 +0800

    添加一个简单的自动测试3

--- osbzr_training/tests/test_lesson_training.py
+++ osbzr_training/tests/test_lesson_training.py
@@ -4,7 +4,7 @@ class TestTraining(TransactionCase):
     def test_create(self):
         self.env['training.lesson'].create(
             {
-               "start_date":"2018-05-20",
+               "start_date":"2018-08-20",
                 "end_date":"2018-06-01",
                 "name":"自动测试创建课程",
             }
